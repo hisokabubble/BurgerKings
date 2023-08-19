8f3e23eba323eaefc500557285f6d498363c4b34
hisokabubble <s10222087@connect.np.edu.sg>
2023-08-19 21:33:46 +0800

    Done.

--- I3E_BurgerRunners_Project/Assets/Scripts/States/BurgerState.cs
+++ I3E_BurgerRunners_Project/Assets/Scripts/States/BurgerState.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AttackState : BaseState
+public class BurgerState : BaseState
 {
     private float moveTimer;
     private float losePlayerTimer;
     private float shotTimer;
     public override void Enter()
     {
-        
+
     }
 
     public override void Exit()
     {
-        
+
     }
 
     public override void Perform()
@@ -26,11 +26,11 @@ public class AttackState : BaseState
             moveTimer += Time.deltaTime;
             shotTimer += Time.deltaTime;
             enemy.transform.LookAt(enemy.Player.transform);
-            if(shotTimer > enemy.fireRate)
+            if (shotTimer > enemy.fireRate)
             {
                 Shoot();
             }
-            if(moveTimer > Random.Range(3, 9))
+            if (moveTimer > Random.Range(3, 9))
             {
                 enemy.Agent.SetDestination(enemy.transform.position + (Random.insideUnitSphere * 5));
                 moveTimer = 0;
@@ -39,7 +39,7 @@ public class AttackState : BaseState
         else
         {
             losePlayerTimer += Time.deltaTime;
-            if(losePlayerTimer > 10)
+            if (losePlayerTimer > 10)
             {
                 stateMachine.ChangeState(new PatrolState());
             }
@@ -49,13 +49,12 @@ public class AttackState : BaseState
     public void Shoot()
     {
         Transform gunbarrel = enemy.gunBarrel;
-        GameObject bullet = GameObject.Instantiate(Resources.Load("Prefabs/Bullet") as GameObject, gunbarrel.position, enemy.transform.rotation);
-        GameObject vfx = GameObject.Instantiate(Resources.Load("Prefabs/Eclipse_VFX") as GameObject, gunbarrel.position, enemy.transform.rotation);
+        GameObject bullet = GameObject.Instantiate(Resources.Load("Prefabs/Eclipse_VFX") as GameObject, gunbarrel.position, enemy.transform.rotation);
         Vector3 shootDirection = (enemy.Player.transform.position - gunbarrel.transform.position).normalized;
         bullet.GetComponent<Rigidbody>().velocity = Quaternion.AngleAxis(Random.Range(-2f, 2f), Vector3.up) * shootDirection * 40;
         Debug.Log("Shoot");
         shotTimer = 0;
     }
 
-    
+
 }
